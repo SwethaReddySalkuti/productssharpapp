f75728f982bfba2aea6ce5a0be1dc470d28022c1
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-04-30 18:04:01 +0530

    Delete from db

--- models/product.js
+++ models/product.js
@@ -18,7 +18,9 @@ module.exports = class Product {
     );
   }
 
-  static deleteById(id) {}
+  static deleteById(id) {
+    return db.execute('DELETE FROM new_table WHERE new_table.id = ?',[id]);
+  }
 
   static fetchAll() {
     return db.execute('SELECT * FROM new_table');
